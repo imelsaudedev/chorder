36304eb630ba1f31f31db4c73ea4db91deee7ce1
Toshi Kurauchi <andrew.kurauchi@gmail.com>
2023-10-07 13:21:10 -0500

    Fix chord/lyrics position when one of them is missing

--- components/ChordProViewer/index.tsx
+++ components/ChordProViewer/index.tsx
@@ -1,9 +1,10 @@
-import { Line } from "chordsheetjs";
+import { Line, Song } from "chordsheetjs";
 import styles from "./styles.module.scss";
 import { parseChordPro } from "../../chopro/music";
+import { useMemo } from "react";
 
 export default function ChordProViewer({ chordpro }: { chordpro: string }) {
-  const chordproHtml = parseChordPro(chordpro);
+  const chordproHtml = useMemo<Song>(() => parseChordPro(chordpro), [chordpro]);
   return (
     <div>
       {chordproHtml.lines.map((line, idx) => (
@@ -16,27 +17,61 @@ export default function ChordProViewer({ chordpro }: { chordpro: string }) {
 function ChordProLine({ line }: { line: Line }) {
   return (
     <div
-      className={`flex flex-row flex-wrap items-end relative`}
+      className={`flex flex-row flex-wrap relative`}
       style={{ breakInside: "avoid" }}
     >
       {line.items.length === 0 && <br />}
       {line.items.map((item, elementIdx) => (
-        <ChordProItem item={item} key={`song-line-item-${elementIdx}`} />
+        <ChordProItem
+          item={item}
+          key={`song-line-item-${elementIdx}`}
+          isConnection={isConnection(line.items, elementIdx)}
+        />
       ))}
     </div>
   );
 }
 
-function ChordProItem({ item }: { item: any }) {
+function ChordProItem({
+  item,
+  isConnection,
+}: {
+  item: any;
+  isConnection: boolean;
+}) {
   if (item._name === "comment") {
     return <span className="italic">{item._value}</span>;
   }
+
+  const chordClasses = ["mr-1", "leading-none", "font-bold", "mb-0"];
+  if (!item.chords.trim()) {
+    chordClasses.push("flex-grow");
+  }
+
+  const lyricsClasses = ["leading-none", styles.lyrics];
+  if (isConnection) {
+    lyricsClasses.push(styles.lyricsConnection);
+  }
+  if (!item.lyrics.trim()) {
+    lyricsClasses.push("flex-grow");
+  }
+
   return (
     <div className={"flex flex-col whitespace-pre-wrap mb-2"}>
-      <span className="mr-1 leading-none font-bold mb-0">{item.chords}</span>
-      <span className={`leading-none ${styles.lyrics}`}>
-        {item.lyrics || " "}
-      </span>
+      <span className={chordClasses.join(" ")}>{item.chords}</span>
+      <span className={lyricsClasses.join(" ")}>{item.lyrics || " "}</span>
     </div>
   );
+}
+
+function isConnection(items: any, itemIdx: number) {
+  if (itemIdx === 0) {
+    return false;
+  }
+  const prevItem = items[itemIdx - 1];
+  const currentItem = items[itemIdx];
+  if (prevItem._name === "comment" || currentItem._name === "comment") {
+    return false;
+  }
+  return prevItem.lyrics.trim() && currentItem.lyrics.trim();
 }
