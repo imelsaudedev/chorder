8b021406301cf7cd05bab01a9c83d8091f9d0b00
Toshi Kurauchi <andrew.kurauchi@gmail.com>
2024-07-11 21:49:51 +0000

    Add transpose to service

--- fragments/ServiceViewPage/ServiceSongUnitView.tsx
+++ fragments/ServiceViewPage/ServiceSongUnitView.tsx
@@ -1,6 +1,8 @@
 import { ServiceSongUnit } from '@/models/service-unit';
 import ArrangementView from '../ArrangementViewPage/ArrangementView';
 import { getSongUnitMap } from '@/models/song-arrangement';
+import KeyButtonSet from '@/components/KeyButtonSet';
+import { useState } from 'react';
 
 type ServiceSongUnitViewProps = {
   unit: ServiceSongUnit;
@@ -8,16 +10,25 @@ type ServiceSongUnitViewProps = {
 };
 
 export default function ServiceSongUnitView({ unit, columns }: ServiceSongUnitViewProps) {
+  const [transpose, setTranspose] = useState(unit.song.arrangement.semitoneTranspose);
   const arrangement = unit.song.arrangement;
   return (
     <div className="w-full">
-      <h2 className="font-bold text-lg leading-none">{unit.song.title}</h2>
-      {unit.song.artist && <span className="text-sm text-gray-400 leading-tight mb-1">{unit.song.artist}</span>}
+      <div className="flex gap-2 flex-grow justify-between items-center mb-2">
+        <div className="flex flex-col">
+          <h2 className="font-bold text-lg leading-none text-primary">{unit.song.title}</h2>
+          {unit.song.artist && <span className="text-sm text-muted">{unit.song.artist}</span>}
+        </div>
+        <div className="flex gap-2">
+          <KeyButtonSet originalKey={arrangement.key || ''} transpose={transpose} setTranspose={setTranspose} />
+        </div>
+      </div>
+
       <ArrangementView
         songUnitMap={getSongUnitMap(arrangement)}
         songKey={arrangement.key}
         columns={columns}
-        transpose={unit.song.arrangement.semitoneTranspose}
+        transpose={transpose}
       />
     </div>
   );
