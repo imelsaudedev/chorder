191393798b36c16e05d715efd0b284c5804f0349
Tiago da Costa <tiagodacosta01@gmail.com>
2025-03-14 19:44:41 +0000

    Ajuste no botão "Mover arranjo" e correção de uma classe incompleta.

--- fragments/UnitForm/index.tsx
+++ fragments/UnitForm/index.tsx
@@ -85,7 +85,7 @@ export default function UnitForm({ unit, removeUnit, onChangeUnit, className }: 
         <div className="flex flex-col">
           <TextInput
             id={contentId}
-            className="resize-none flex-grow bg-white font-mono ling"
+            className="resize-none flex-grow bg-white font-mono"
             placeholder={t('UnitData.contentPlaceholder')}
             onChange={handleChangeChordpro}
             value={unit.content}
