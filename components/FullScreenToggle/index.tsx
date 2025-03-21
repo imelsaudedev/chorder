7646ee8eef519089a7efb2eb4232806ae512f6f6
Tiago da Costa <tiagodacosta01@gmail.com>
2025-03-21 19:18:24 +0000

    Altera o visual do botão de fullscreen

--- components/FullScreenToggle/index.tsx
+++ components/FullScreenToggle/index.tsx
@@ -28,15 +28,15 @@ export default function FullscreenToggle({ onPrevious, onNext }: FullscreenToggl
     <div className="fixed bottom-4 right-4 md:bottom-8 md:right-8 flex items-center gap-2 z-50">
       {isFullScreen && (
         <div className="flex gap-2">
-          <Button variant="outline" size="icon" onClick={onPrevious}>
+          <Button variant="default" size="icon" onClick={onPrevious}>
             <ChevronUp size={24} />
           </Button>
-          <Button variant="outline" size="icon" onClick={onNext}>
+          <Button variant="default" size="icon" onClick={onNext}>
             <ChevronDown size={24} />
           </Button>
         </div>
       )}
-      <Button circle variant="outline" size="icon" onClick={toggleFullScreen}>
+      <Button circle variant="secondary" size="square" rounded="full" onClick={toggleFullScreen}>
         {isFullScreen ? <Minimize size={24} /> : <Maximize size={24} />}
       </Button>
     </div>
