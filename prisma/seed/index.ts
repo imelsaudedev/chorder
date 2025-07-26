012a2289748f29f50ab073a7fb9c7b4b8d4fc95f
Toshi Kurauchi <andrew.kurauchi@gmail.com>
2025-07-26 22:52:28 +0000

    Seed again

--- prisma/seed/index.ts
+++ prisma/seed/index.ts
@@ -3,6 +3,36 @@ import { addNewServices, addNewSongs } from "./legacy-data";
 import { createServicesFromLocal, createSongsFromLocal } from "./local-data";
 
 export async function main() {
+  console.log("Deleting old services...");
+  await prisma.service.deleteMany({
+    where: { date: { lt: new Date("2025-07-15") } },
+  });
+  console.log("Deleting song arrangements without service unit...");
+  const serviceArrangementsWithoutUnit = await prisma.songArrangement.findMany({
+    where: { isServiceArrangement: true, serviceUnit: null },
+  });
+  console.log(
+    `Found ${serviceArrangementsWithoutUnit.length} arrangements without service unit.`
+  );
+  if (serviceArrangementsWithoutUnit.length > 0) {
+    // Split in chunks of 10000 to avoid too large delete queries
+    const chunks = [];
+    for (let i = 0; i < serviceArrangementsWithoutUnit.length; i += 10000) {
+      chunks.push(serviceArrangementsWithoutUnit.slice(i, i + 10000));
+    }
+    for (const chunk of chunks) {
+      console.log("Deleting 10000 arrangements in chunks...");
+      await prisma.songArrangement.deleteMany({
+        where: {
+          id: { in: chunk.map((arr) => arr.id) },
+        },
+      });
+    }
+    console.log("Deleted service arrangements without service unit.");
+  } else {
+    console.log("No service arrangements without service unit found.");
+  }
+  console.log("Done");
   const songs = await createSongsFromLocal(prisma);
   await createServicesFromLocal(prisma, songs);
 
